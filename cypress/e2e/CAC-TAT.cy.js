b3ac95e3006f58fe41128ba080f9f4eadf072241
Guilherme Barbosa <guilherme.dourado95@gmail.com>
2025-01-30 20:16:40 -0300

    subindo segundo exercicio

--- cypress/e2e/CAC-TAT.cy.js
+++ cypress/e2e/CAC-TAT.cy.js
@@ -1,6 +1,19 @@
-describe('template spec', () => {
-  it('passes', () => {
+/// <reference types="Cypress" />
+
+describe('Central de atendimento ao Cliente TAT', () => {
+  beforeEach(() => {
     cy.visit('./src/index.html')
+  })
+  it('testa que titulo da pagina esta correto', () => {
     cy.title().should('eq', 'Central de Atendimento ao Cliente TAT')
+  })
+  it('preenche os campos obrigatórios e envia o formulário', () => {
+    cy.get('#firstName').type('Creuzo')
+    cy.get('#lastName').type('Ronaldo')
+    cy.get('#email').type('creuzo.ronaldo@email.com')
+    cy.get('#phone').type('11998766655')
+    cy.get('#open-text-area').type('nope')
+    cy.get('#white-background > form > button').click()
+    cy.get('.success > strong').should('be.visible')
   })
 })
